29981caa00219701581a1cb2c3f0fa656bd60193
aniketdahifale2766-debug <aniketdahifale2766@gmail.com>
2026-08-23 04:38:55 +0530

    Bump app version to 1.0.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.audio.streamlan"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.0.1"
+        versionCode = 4
+        versionName = "1.0.2"
     }
 
     buildTypes {
